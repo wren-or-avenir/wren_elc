ee15f642a5154d4dd40aecf91cb1af45b2f5b35a
wren-or-avenir <Ydy2398733419@outlook.com>
2026-04-13 13:19:35 +0800

    新增动态dt,新增update,重构predict函数,修改了关于卡尔曼增益的注释

--- src/models/Kalman.py
+++ src/models/Kalman.py
@@ -7,12 +7,12 @@ class KalmanFilter:
         q_scale: 过程噪声缩放因子（信任模型的程度）
         r_scale: 测量噪声缩放因子（信任传感器的程度）
         卡尔曼增益k:  约等于p/(p+r)
-        q/r越大,k越小,滤波器越依赖测量值(更灵敏);
-        q/r越小,k越大,滤波器越依赖预测值(更平稳)
+        q/r越大,k越大,滤波器越依赖测量值(更灵敏);
+        q/r越小,k越小,滤波器越依赖预测值(更平稳)
         """
         self.q_scale = q_scale 
         self.r_scale = r_scale
-        self.dt = 0.033     # 先用0.1  (30 FPS 约等于 0.033s)
+        self.dt = 0.033     
         
         # 状态维度2(位置,速度)，观测维度1(位置)
         self.kf = cv2.KalmanFilter(2, 1)
@@ -38,17 +38,24 @@ class KalmanFilter:
         # 初始化协方差为较大值，确保第一帧能迅速锁死目标
         self.reset()
 
-    def predict(self):
+    def predict(self, dt=None):
+        if dt is not None:
+            self.dt = dt
+            # 动态更新状态转移矩阵中的 dt 部分
+            self.kf.transitionMatrix[0, 1] = dt
         prediction = self.kf.predict()
-        return prediction[0, 0]  # 返回预测的位置值
+        return prediction[0, 0] , prediction[1, 0] # 返回预测的位置值
 
     def update(self, measurement):
         measure = np.array([[np.float32(measurement)]])
         estimate = self.kf.correct(measure)
-        return estimate[0, 0]  # 返回修正后的最优位置值
+        return estimate[0, 0] , estimate[1, 0] # 返回修正后的最优位置值
 
     def reset(self):
         # 状态置零，初始化
         self.kf.statePost = np.zeros((2, 1), np.float32)
         # 协方差重置为大值：告诉滤波器“我现在完全不确定位置”，强制它快速收敛到第一帧观测值
         self.kf.errorCovPost = np.eye(2, dtype=np.float32) * 1000.0
+
+    def get_state(self):
+        return self.kf.statePost[0, 0], self.kf.statePost[1, 0]
